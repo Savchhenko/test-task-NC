0a0618c4c17af7bba67fd8533bc1ead57d62abbd
Alexandra Savchenko <svchhh@yandex.ru>
2022-10-19 22:48:17 +0300

    add task6 - centering

---
+++ task6/main.js
@@ -0,0 +1,6 @@
+const parentElem = document.querySelector(".parent__via-js");
+const childElem = document.querySelector(".child__via-js");
+
+// Центрирование через JS
+parentElem.style.display = "flex";
+childElem.style.margin = "auto";
